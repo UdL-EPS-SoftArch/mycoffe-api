3c9335ae64c44e4050a5099a3fdfd1eeeef3893f
Jordipifarre <jordi.pifarre.ribes@gmail.com>
2025-09-24 18:35:19 +0200

    Refactor ProductRepository: update query methods to use BigDecimal for price and improve naming for clarity

--- src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
@@ -10,11 +10,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByName(String name);
     List<Product> findByBrand(String brand);
-    List<Product> findByPriceBetween(double min, double max);
-    List<Product> findByRatingGreaterThanEqual(double rating);
-    List<Product> findByIsAvailableTrue();
-    List<Product> findByIsAvailableFalse();
-    List<Product> findByOrder(Order order);
+    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+    List<Product> findByRatingGreaterThanEqual(Double rating);
+    List<Product> findByAvailable(boolean available);
+    List<Product> findByOrders(Order order);
     List<Product> findByBaskets(Basket basket);
     List<Product> findByPromotions( String promotion);
     List<Product> findBySize(String size);
