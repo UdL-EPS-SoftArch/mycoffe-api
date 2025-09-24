f2ab62045629582f9c540e534fae8b7fd688b786
Jordipifarre <jordi.pifarre.ribes@gmail.com>
2025-09-24 18:35:01 +0200

    Refactor ProductRepository: change ID type from String to Long for improved database consistency

--- src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
@@ -2,12 +2,11 @@ package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByName(String name);
     List<Product> findByBrand(String brand);
