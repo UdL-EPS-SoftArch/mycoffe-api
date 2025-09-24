c310c8b5252c04243e5a143b794a40ccb37ad954
HP <krisoc123@gmail.com>
2025-09-24 18:04:04 +0200

    entities

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Customer.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Customer extends User {
+
+    @NotEmpty
+    private String phoneNumber;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    @Override
+    @ElementCollection
+    public Collection<GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_CUSTOMER");
+    }
+}
