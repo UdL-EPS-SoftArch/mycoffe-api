34b1983ae8967694bbdf71a9de3a97800e513549
Daniel Resa Perez <drp5@alumnes.udl.cat>
2025-09-25 16:43:41 +0200

    Fix.

--- src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ProductRepository.java
@@ -1,23 +1,29 @@
 package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 import java.math.BigDecimal;
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+@RepositoryRestResource
+public interface ProductRepository extends CrudRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
+
 
     List<Product> findByName(String name);
     List<Product> findByBrand(String brand);
     List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
     List<Product> findByRatingGreaterThanEqual(Double rating);
-    List<Product> findByAvailable(boolean available);
+    List<Product> findByIsAvailable(boolean available);
     List<Product> findByPromotions( String promotion);
     List<Product> findBySize(String size);
     List<Product> findByKcalLessThanEqual(int kcal);
     List<Product> findByIngredientsContaining(String ingredient);
     List<Product> findByAllergensContaining(String allergen);
+
     //TODO  List<Product> findByOrders(Order order);
     //      List<Product> findByBaskets(Basket basket);
 
