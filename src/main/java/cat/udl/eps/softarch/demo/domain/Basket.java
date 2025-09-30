3b169447fc4428e391737fae7ac4375b3bcccf94
Malena <malenaudl@gmail.com>
2025-09-30 16:47:05 +0200

    Añadida clase Basket con relación OneToOne con Customer

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Basket.java
@@ -0,0 +1,34 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collection;
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Basket extends UriEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Dueño de la relación
+    @OneToOne
+    @JoinColumn(name = "customer_id", unique = true)
+    private Customer customer;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+}
