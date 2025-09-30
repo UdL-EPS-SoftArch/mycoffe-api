a5967f0a5526c3a193346a63a883e753f6258165
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-09-30 16:28:53 +0200

    Update Business
    New Columns:
    Address
    Name

--- src/main/java/cat/udl/eps/softarch/demo/domain/Business.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Business.java
@@ -1,41 +1,62 @@
 package cat.udl.eps.softarch.demo.domain;
-import jakarta.persistence.*;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 @Entity
 @Table(name = "business")
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Business {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false)
+    private String address;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BusinessStatus status;
 
-    // Constructor vacío
     public Business() {}
 
-    // Constructor con parámetros
-    public Business(BusinessStatus status) {
+    public Business(String name, String address, BusinessStatus status) {
+        this.name = name;
+        this.address = address;
         this.status = status;
     }
 
-    // Getters y Setters
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public BusinessStatus getStatus() {
@@ -46,4 +67,3 @@ public class Business {
         this.status = status;
     }
 }
-
