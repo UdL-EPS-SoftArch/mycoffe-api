953f97fc7db0c3e9314b58647fcae1cfa393ce4e
wellin <baezrwellingtons@gmail.com>
2025-10-01 10:23:07 +0200

    fix: Replaced the cardNumber field to be completely id

--- src/main/java/cat/udl/eps/softarch/demo/repository/LoyaltyRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/LoyaltyRepository.java
@@ -5,7 +5,6 @@ import cat.udl.eps.softarch.demo.domain.Customer;
 // import cat.udl.eps.softarch.demo.domain.Business; // Uncomment after implementing the business class
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -14,7 +13,7 @@ import java.util.Optional;
 @RepositoryRestResource
 public interface LoyaltyRepository extends PagingAndSortingRepository<Loyalty, Long>, CrudRepository<Loyalty, Long> {
     // Optional<Loyalty> findByCustomerAndBusiness(@Param("customer") Customer customer, @Param("business") Business business); // Uncomment after implementing the business class
-    Optional<Loyalty> findByCardNumber(@Param("cardNumber") String cardNumber);
+    Optional<Loyalty> findById(Long id);
 
     // List<Loyalty> findByBusiness(Business business); // Uncomment after implementing the business class
     List<Loyalty> findByCustomer(Customer customer);
