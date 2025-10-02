3b6a86420e59791c15cdff21635c06704248a7b0
Malena <malenaudl@gmail.com>
2025-10-02 15:25:04 +0200

    updated files

--- src/main/java/cat/udl/eps/softarch/demo/domain/Basket.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Basket.java
@@ -2,12 +2,7 @@ package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,7 +19,7 @@ public class Basket extends UriEntity<Long> {
 
     // Dueño de la relación
     @NotNull
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "customer_id", unique = true)
     @JsonIdentityReference(alwaysAsId = true)
     private Customer customer;
--- src/main/java/cat/udl/eps/softarch/demo/repository/BasketRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/BasketRepository.java
@@ -1,4 +1,7 @@
 package cat.udl.eps.softarch.demo.repository;
 
-public interface BasketRepository {
+import cat.udl.eps.softarch.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BasketRepository extends JpaRepository<User, Long> {
 }
