fba4a70ba560365be3a42a78de0e6f57e0def4a6
Oriol-Farras <oriolfarras2004@gmail.com>
2025-10-07 14:34:35 +0200

    feat(domain): Implement Business to Loyalty and Inventory relationships

--- src/main/java/cat/udl/eps/softarch/demo/domain/Business.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Business.java
@@ -2,14 +2,18 @@ package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import java.util.ArrayList;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -30,4 +34,11 @@ public class Business extends User {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_BUSINESS");
     }
+
+    @OneToMany(mappedBy = "business", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Loyalty> loyaltyPrograms = new ArrayList<>();
+
+    @OneToMany(mappedBy = "business", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Inventory> inventories = new ArrayList<>();
+
 }
