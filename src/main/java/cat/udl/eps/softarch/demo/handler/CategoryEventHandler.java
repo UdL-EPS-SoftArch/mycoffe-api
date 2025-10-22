f2ebf86f7198f9161fd3f577f64ef203cc33263c
Eric Gil <egc20@alumnes.udl.cat>
2025-10-22 17:45:45 +0200

    fix: resolve import conflict for AccessDeniedException in CategoryEventHandler

--- src/main/java/cat/udl/eps/softarch/demo/handler/CategoryEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/demo/handler/CategoryEventHandler.java
@@ -5,12 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.nio.file.AccessDeniedException;
 
 @Component
 @RepositoryEventHandler
