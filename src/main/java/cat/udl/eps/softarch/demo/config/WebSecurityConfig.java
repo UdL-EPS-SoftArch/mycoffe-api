8444e611fd210beb1001ef38212661794ad7b43c
Oriol-Farras <oriolfarras2004@gmail.com>
2025-10-30 20:34:11 +0100

    fix(config): resolve ApplicationContext load failure by adding PasswordEncoder

--- src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -64,5 +66,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
         return new SecurityEvaluationContextExtension();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
